b6d733f97f6c90f69d9e4f1f8904668b089509d8
Aakanksha Dhurandhar <34390765+aakankshadhurandhar@users.noreply.github.com>
2021-07-06 09:18:20 +0530

    added logo in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import './Navbar.css'
 export const  Navbar=()=>{
     return(
        <div className="header_container">
+           <div className="logo_container">
+           <img src="../spacex_logo.png" alt="spacex_logo" />
+           </div>
             
        </div>
     )
