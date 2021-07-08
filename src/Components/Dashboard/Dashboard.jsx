018be243e79fa62e9200d5febaab4c5f9322525d
Aakanksha Dhurandhar <34390765+aakankshadhurandhar@users.noreply.github.com>
2021-07-08 12:19:24 +0530

    Seperated table and made it into another component

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Table}  from "semantic-ui-react"
+
 import { Filterbystatus } from '../Filterbystatus';
+import { LaunchList } from '../Launchlist';
 import './Dashboard.css'
 
 function Dashboard(){
@@ -12,53 +13,7 @@ return (
            <Filterbystatus />
        </div>
        <div className="table_container">
-           <Table singleLine>
-                    <Table.Header>
-                    <Table.Row>
-						<Table.HeaderCell>Header</Table.HeaderCell>
-						<Table.HeaderCell>Header</Table.HeaderCell>
-						<Table.HeaderCell>Header</Table.HeaderCell>
-						<Table.HeaderCell>Header</Table.HeaderCell>
-						<Table.HeaderCell>Header</Table.HeaderCell>
-						<Table.HeaderCell>Header</Table.HeaderCell>
-						<Table.HeaderCell>Header</Table.HeaderCell>
-						
-						
-					</Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-							<Table.Row>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								
-							</Table.Row>
-							<Table.Row>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								
-							</Table.Row>
-							<Table.Row>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								<Table.Cell>Cell</Table.Cell>
-								
-							</Table.Row>
-						</Table.Body>
-           </Table>
+           <LaunchList />
        </div>
     </div>
 )
