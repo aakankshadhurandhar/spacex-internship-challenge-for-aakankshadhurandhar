a51800a045aaf52f908e18276ef3e5b5c0bc38c7
Aakanksha Dhurandhar <34390765+aakankshadhurandhar@users.noreply.github.com>
2021-07-12 09:04:04 +0530

    Added  onclick functionality on row

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,51 +1,36 @@
-import React,{useState} from 'react';
+import React from 'react';
 
 import { Filterbystatus } from '../Filterbystatus';
 import { LaunchList } from '../Launchlist';
-import Model from '../Model/Model';
+
 import './Dashboard.css'
 
-function Dashboard(){
-    const[Modelisopen,setModelisopen]=useState(false)
-    const[launchdetails,setlaunchdetails]=useState({})
-    const handleClose=()=>setModelisopen(false)
-    const handleopen=()=>setModelisopen(true)
-    const modeldetails=(details)=>setlaunchdetails(details)
-return (
-    
-    <>
-    <button onClick={handleopen}>Open</button>
-    {
-        Modelisopen?(
-            <Model
-            modelstatus={Modelisopen}
-            handleClose={handleClose}
-            launchdetails={launchdetails}
-            />
-        )
+function Dashboard() {
 
-        :
-        (
+    return (
+
+        <>
+
             <div className="dashboard_container">
 
 
-      <div className="filter_section">
-           <Filterbystatus />
-       </div>
-       <div className="table_container">
-           <LaunchList getmodaldetails={(x)=>modeldetails(x)} />
-       </div>
+                <div className="filter_section">
+                    <Filterbystatus />
+                </div>
+                <div className="table_container">
+                    <LaunchList />
+                </div>
 
-</div>
-        )
-
-
-    }
+            </div>
 
 
-    
-     
-    </>
-)
+
+
+
+
+
+
+        </>
+    )
 }
 export default Dashboard;
--- src/Components/Model/Model.jsx
+++ src/Components/Model/Model.jsx
@@ -1,50 +1,49 @@
-import React from 'react';
-import ReactModal from 'react-modal'
-
-
+import React from "react";
+import ReactModal from "react-modal";
 
 ReactModal.setAppElement("#root");
-
-
-function Model({modelstatus,handleClose,launchdetails}){
-   
-    return (
-        <ReactModal
-        style={{
-            overlay: {
-                position: "fixed",
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                backgroundColor: "rgba(255, 255, 255, 0.75)",
-                zIndex: 1000,
-            },
-            content: {
-                position: "absolute",
-                top: "40px",
-                left: "40px",
-                right: "40px",
-                bottom: "40px",
-                border: "1px solid #ccc",
-                background: "#fff",
-                overflow: "auto",
-                WebkitOverflowScrolling: "touch",
-                borderRadius: "4px",
-                outline: "none",
-                padding: "20px",
-                width: "500px",
-                height: "250px",
-                margin: "0 auto",
-            },
-        }}
-        isOpen={modelstatus}
-        onRequestClose={handleClose}
-    >
-        <h2>flight2</h2>
-        <p>Modal Body</p>
-        <button onClick={handleClose}>Close Modal</button>
-    </ReactModal>
-    )
+function Model({ modalStatus, handleClose, launchDetails }) {
+	return (
+		<ReactModal
+			style={{
+				overlay: {
+					position: "fixed",
+					top: 0,
+					left: 0,
+					right: 0,
+					bottom: 0,
+					backgroundColor: "transparent",
+					zIndex: 1000,
+				},
+				content: {
+					position: "absolute",
+					left: "50%",
+					transform: "translateX(-50%)",
+					border: "1px solid #ccc",
+					background: "#fff",
+					overflow: "auto",
+					WebkitOverflowScrolling: "touch",
+					borderRadius: "6px",
+					outline: "none",
+					width: "700px",
+					height: "500px",
+					padding: "0px !important",
+					margin: "0 auto",
+				},
+			}}
+			isOpen={modalStatus}
+			onRequestClose={handleClose}
+		>
+			<div className="modal-container">
+				<div className="modal-header">
+					<p className="mission-heading">Flight</p>
+					<button onClick={handleClose}>X</button>
+				</div>
+				<h2>{launchDetails}</h2>
+				<p>Modal Body</p>
+			</div>
+		</ReactModal>
+	);
 }
+
 export default Model;
